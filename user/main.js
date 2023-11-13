fb493d4dfaefafdf2d2a0a23685683c677740f3a
Mary0711 <mariliana.barrientos@upr.edu>
2023-11-13 00:20:46 -0400

    user fron end finished

---
+++ user/main.js
@@ -0,0 +1,8 @@
+function showPopup() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "block";
+}
+function goBack() {
+  var popup = document.getElementById("popup");
+  popup.style.display = "none";
+}
