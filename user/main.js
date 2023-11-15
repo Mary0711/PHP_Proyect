fa1a52e8f79571518369eda54dcf4b2de7184a04
Mary0711 <87920655+Mary0711@users.noreply.github.com>
2023-11-15 13:59:16 -0400

    add registro

--- user/main.js
+++ user/main.js
@@ -1,8 +1,25 @@
+function showPopupCurso() {
+  var popup = document.getElementById("popupCurso");
+  popup.style.display = "block";
+}
+function goBackCurso() {
+  var popup = document.getElementById("popupCurso");
+  popup.style.display = "none";
+}
+
+function showPopupEst() {
+  var popup = document.getElementById("popupEstudiante");
+  popup.style.display = "block";
+}
+function goBackEst() {
+  var popup = document.getElementById("popupEstudiante");
+  popup.style.display = "none";
+}
 function showPopup() {
-  var popup = document.getElementById("popup");
+  var popup = document.getElementById("popupCurso");
   popup.style.display = "block";
 }
 function goBack() {
-  var popup = document.getElementById("popup");
+  var popup = document.getElementById("popupCurso");
   popup.style.display = "none";
 }
