f96d1344d214d91582c8363f42c3f9a4a6ae7785
Mary0711 <mariliana.barrientos@upr.edu>
2023-11-16 01:34:49 -0400

    admin almost finished, few details left

--- user/main.js
+++ user/main.js
@@ -1,25 +1,47 @@
-function showPopupCurso() {
-  var popup = document.getElementById("popupCurso");
-  popup.style.display = "block";
-}
-function goBackCurso() {
-  var popup = document.getElementById("popupCurso");
-  popup.style.display = "none";
-}
-
-function showPopupEst() {
-  var popup = document.getElementById("popupEstudiante");
-  popup.style.display = "block";
-}
-function goBackEst() {
-  var popup = document.getElementById("popupEstudiante");
-  popup.style.display = "none";
-}
 function showPopup() {
-  var popup = document.getElementById("popupCurso");
+  var popup = document.getElementById("popup");
   popup.style.display = "block";
 }
 function goBack() {
-  var popup = document.getElementById("popupCurso");
+  var popup = document.getElementById("popup");
   popup.style.display = "none";
+}
+
+function showForm() {
+  var courseForm = document.getElementById("courseForm");
+  courseForm.style.display = "block";
+}
+function cancelForm() {
+  var courseForm = document.getElementById("courseForm");
+  courseForm.style.display = "none";
+}
+function showCourse() {
+  var editCourse = document.getElementById("editCourse");
+  editCourse.style.display = "block";
+}
+
+function cancelCourse() {
+  var editCourse = document.getElementById("editCourse");
+  editCourse.style.display = "none";
+}
+
+function showList() {
+  var list = document.getElementById("List");
+  list.style.display = "block";
+}
+
+function cancelList() {
+  var list = document.getElementById("List");
+  list.style.display = "none";
+}
+
+function showTable(tableId) {
+  var tables = document.querySelectorAll(".results table");
+  tables.forEach(function (table) {
+    if (table.id === tableId) {
+      table.style.display = "table";
+    } else {
+      table.style.display = "none";
+    }
+  });
 }
